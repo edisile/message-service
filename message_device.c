d8fe75e3a46baec949e5aa1ffe699a3507561766
Eduard <e@e.it>
2020-02-01 16:16:02 +0100

    Added module parameters in /sys/

--- message_device.c
+++ message_device.c
@@ -1,6 +1,7 @@
+#include <linux/errno.h>
+#include <linux/fs.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 #include <linux/version.h>
@@ -22,6 +23,13 @@ static ssize_t dev_read(struct file *filp, char *buff, size_t len, loff_t *off);
 // Globals
 static int MAJOR;
 DEFINE_LF_QUEUE(queue);
+static long max_message_size = 512;
+static long max_storage_size = 4096;
+static atomic_t stored_bytes = {.counter = 0};
+
+// Module parameters exposed via the /sys/ pseudo-fs
+module_param(max_message_size, long, 0660);
+module_param(max_storage_size, long, 0660);
 
 // Driver implementation
 static int dev_open(struct inode *inode, struct file *file) {
@@ -43,7 +51,8 @@ struct queue_elem {
 static ssize_t dev_write(struct file *filp, const char *buff, size_t len, 
 			loff_t *off) {
 	struct queue_elem *elem;
-	ssize_t ret;
+	unsigned long failed;
+	ssize_t retval;
 
 	#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 0, 0)
 		printk("%s: write on [%d,%d]\n", MODNAME, 
@@ -55,23 +64,30 @@ static ssize_t dev_write(struct file *filp, const char *buff, size_t len,
 			MINOR(filp->f_dentry->d_inode->i_rdev));
 	#endif
 
+	if (len > min(max_message_size, max_storage_size - stored_bytes.counter)) {
+		retval = -ENOSPC;
+		goto exit;
+	}
+
 	// Allocation could block because GFP_KERNEL but it's not a problem
 	elem = kmalloc(sizeof(struct queue_elem), GFP_KERNEL);
 	elem->message = kmalloc(len, GFP_KERNEL);
 
 	printk("	%lu bytes to write", len);
-	ret = copy_from_user(elem->message, buff, len);
-	elem->message_len = (len - ret);
+	failed = copy_from_user(elem->message, buff, len);
+	retval = elem->message_len = len - failed;
 	lf_queue_push(&queue, (&(elem->list)));
+	atomic_add(retval, &stored_bytes); // Keep track of used space in device
 	
-	return (len - ret);
+	exit:
+	return retval;
 }
 
 static ssize_t dev_read(struct file *filp, char *buff, size_t len, 
 			loff_t *off) {
 	struct lf_queue_node *node;
 	struct queue_elem *elem;
-	ssize_t ret = 0;
+	ssize_t retval = 0;
 
 	#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 0, 0)
 		printk("%s: read on [%d,%d]\n",
@@ -87,16 +103,17 @@ static ssize_t dev_read(struct file *filp, char *buff, size_t len,
 
 	if (node != NULL) {
 		elem = container_of(node, struct queue_elem, list);
-		len = min(len, elem->message_len);
-		ret = copy_to_user(buff, elem->message + *off, len);
+		retval = len = min(len, elem->message_len);
+		printk("	%lu bytes to read", len);
+
+		copy_to_user(buff, elem->message + *off, len);
 	
+		atomic_sub(elem->message_len, &stored_bytes); // Space being freed
 		kfree(elem->message);
 		kfree(elem);
-	} else {
-		len = 0;
 	}
 
-	return len;
+	return retval;
 }
 
 // Driver in a struct
@@ -111,7 +128,7 @@ int init_module(void) {
 	MAJOR = __register_chrdev(0, 0, 256, DEVICE_NAME, &f_ops);
 
 	if (MAJOR < 0) {
-		printk("%s: register failed\n", MODNAME);
+		printk(KERN_ERR "%s: register failed with major %d\n", MODNAME, MAJOR);
 		return MAJOR;
 	}
 
