e1fabfd70f861f5bbe0c82144c47927d78490e23
Eduard <e@e.it>
2020-02-06 11:40:46 +0100

    Added minor management and module cleanup

--- message_device.c
+++ message_device.c
@@ -19,11 +19,9 @@ MODULE_LICENSE("GPL");
 
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 0, 0)
-	#define get_major(session) MAJOR(session->f_inode->i_rdev)
-	#define get_minor(session) MINOR(session->f_inode->i_rdev)
+	#define __MINOR(session) MINOR(session->f_inode->i_rdev)
 	#else
-	#define get_major(session) MAJOR(session->f_dentry->d_inode->i_rdev)
-	#define get_minor(session) MINOR(session->f_dentry->d_inode->i_rdev)
+	#define __MINOR(session) MINOR(session->f_dentry->d_inode->i_rdev)
 #endif
 
 
@@ -56,6 +54,13 @@ struct session_data {
 	struct mutex metadata_lock;
 };
 
+// Data related to a single instance of the file
+struct file_data {
+	struct lf_queue message_queue;
+	atomic_t stored_bytes;
+	struct wait_queue_head wait_queue;
+};
+
 // ioctl commands
 enum ioctl_cmds {
 	SET_SEND_TIMEOUT = 99,
@@ -65,16 +70,17 @@ enum ioctl_cmds {
 
 // Globals
 static int MAJOR;
+#ifndef MINORS
+	#define MINORS 64
+#endif
 static long max_message_size = 512;
 static long max_storage_size = 4096;
-// TODO: move these (vvv) to a struct and make one for each minor
-static DEFINE_LF_QUEUE(queue);
-static DECLARE_WAIT_QUEUE_HEAD(wait_queue); // replace with init_waitqueue_head to init dynamically
-static atomic_t stored_bytes = {.counter = 0};
+static struct file_data files[MINORS];
 
 // Module parameters exposed via the /sys/ pseudo-fs
 module_param(max_message_size, long, 0660);
 module_param(max_storage_size, long, 0660);
+
 
 // Macro to create a struct containing a driver instance
 #define DEFINE_DRIVER_INSTANCE(read_fp, write_fp) ((struct file_operations) { \
@@ -119,16 +125,26 @@ enum driver_mode {
 
 // Opens an I/O session towards the device
 static int dev_open(struct inode *inode, struct file *filp) {
-	// TODO: Check minor number, if too big return -ENODEV
+	int retval = 0;
+
+	if (__MINOR(filp) >= MINORS) {
+		// Minor number not supported
+		retval = -ENODEV;
+		goto exit;
+	}
 
 	// Increment usage count if there's any IO session towards files managed 
 	// by this module
 	if (!try_module_get(THIS_MODULE)) {
-		return -ENODEV; // Module might have been removed
+		// Module might have been removed
+		retval = -ENODEV;
+		goto exit;
 	}
 
 	printk("%s: device file opened\n", MODNAME);
-	return 0;
+
+	exit:
+	return retval;
 }
 
 // Closes an I/O session towards the device
@@ -148,13 +164,14 @@ static int dev_release(struct inode *inode, struct file *filp) {
 // Posts a message on the message queue
 static ssize_t dev_write(struct file *filp, const char *buff, size_t len, 
 						loff_t *off) {
+	struct file_data *d = &files[__MINOR(filp)];
 	struct queue_elem *elem;
 	unsigned long failed;
 	ssize_t retval;
 
-	printk("%s: write on [%d,%d]\n", MODNAME, get_major(filp), get_minor(filp));
+	printk("%s: write on [%d,%d]\n", MODNAME, MAJOR, __MINOR(filp));
 
-	if (len > min(max_message_size, max_storage_size - stored_bytes.counter)) {
+	if (len > min(max_message_size, max_storage_size - d->stored_bytes.counter)) {
 		retval = -ENOSPC;
 		goto exit;
 	}
@@ -175,10 +192,10 @@ static ssize_t dev_write(struct file *filp, const char *buff, size_t len,
 	printk("	%lu bytes to write", len);
 	failed = copy_from_user(elem->message, buff, len);
 	retval = elem->mess_len = len - failed;
-	lf_queue_push(&queue, (&(elem->list)));
-	atomic_add(retval, &stored_bytes); // Keep track of used space in device
+	lf_queue_push(&(d->message_queue), &(elem->list));
+	atomic_add(retval, &(d->stored_bytes)); // Keep track of used space in device
 
-	wake_up(&wait_queue); // Wake up one thread on the wait queue
+	wake_up(&(d->wait_queue)); // Wake up one thread on the wait queue
 	
 	exit:
 	return retval;
@@ -186,24 +203,25 @@ static ssize_t dev_write(struct file *filp, const char *buff, size_t len,
 
 // Reads a message (or a part of one) from the message queue; common 
 // implementation called by all the exposed read variants
-static ssize_t __read_common(struct file *filp, char *buff, size_t len, 
+static ssize_t __read_common(struct file_data *d, char *buff, size_t len, 
 							loff_t *off) {
 	struct lf_queue_node *node;
 	struct queue_elem *elem;
 	ssize_t retval = 0;
 
-	printk("%s: read on [%d,%d]\n", MODNAME, get_major(filp), get_minor(filp));
+	printk("%s: read on [%d,%d]\n", MODNAME, MAJOR, (int) (d - files));
 	
-	node = lf_queue_pull(&queue);
+	node = lf_queue_pull(&(d->message_queue));
 
 	if (node != NULL) {
 		elem = container_of(node, struct queue_elem, list);
 		retval = len = min(len, elem->mess_len);
 		printk("	%lu bytes to read", len);
 
-		copy_to_user(buff, elem->message + *off, len);
+		if (buff != NULL) // buff == NULL means just flushing the message
+			copy_to_user(buff, elem->message + *off, len);
 	
-		atomic_sub(elem->mess_len, &stored_bytes); // Space being freed
+		atomic_sub(elem->mess_len, &(d->stored_bytes)); // Space being freed
 		vfree(elem->message);
 		vfree(elem);
 	}
@@ -214,8 +232,8 @@ static ssize_t __read_common(struct file *filp, char *buff, size_t len,
 // Read a message from the queue; if none is available returns immediately
 static ssize_t dev_read(struct file *filp, char *buff, size_t len, 
 						loff_t *off) {
-	
-	return __read_common(filp, buff, len, off);
+
+	return __read_common(&files[__MINOR(filp)], buff, len, off);
 }
 
 // Read a message from the queue; if none is available wait for a time related 
@@ -225,6 +243,7 @@ static ssize_t dev_read(struct file *filp, char *buff, size_t len,
 // thread
 static ssize_t dev_read_timeout(struct file *filp, char *buff, size_t len, 
 								loff_t *off) {
+	struct file_data *d = &files[__MINOR(filp)];
 	int wait_ret = 0;
 	ssize_t retval = 0;
 	ktime_t entry_time, wakeup_time, timeout;
@@ -245,14 +264,15 @@ static ssize_t dev_read_timeout(struct file *filp, char *buff, size_t len,
 		goto exit;
 	}
 
-	if (queue.head == NULL) // 
-		wait_ret = wait_event_hrtimeout(wait_queue, (queue.head != NULL), 
-										timeout);
+	if (d->message_queue.head == NULL) {
+		wait_ret = wait_event_hrtimeout(d->wait_queue, 
+										d->message_queue.head != NULL, timeout);
+	}
 
 	if (wait_ret == 0) {
 		// Condition has become true, try to read
 		wakeup_time = ktime_get();
-		retval = __read_common(filp, buff, len, off);
+		retval = __read_common(d, buff, len, off);
 
 		if (retval != 0)
 			goto exit; // Success, return
@@ -354,6 +374,7 @@ static int dev_flush(struct file *filp, void *id) {
 }
 
 int init_module(void) {
+	int i;
 	MAJOR = __register_chrdev(0, 0, 256, DEVICE_NAME, &f_ops[R_W]);
 
 	if (MAJOR < 0) {
@@ -362,11 +383,33 @@ int init_module(void) {
 	}
 
 	printk(KERN_INFO "%s: registered as %d\n", MODNAME, MAJOR);
+
+	// Initialize the data struct for all possible file instances
+	for (i = 0; i < MINORS; i++) {
+		files[i] = (struct file_data) {
+			.message_queue = NEW_LF_QUEUE,
+			.stored_bytes = ATOMIC_INIT(0)
+		};
+
+		init_waitqueue_head(&(files[i].wait_queue));
+	}
+
 	return 0;
 }
 
 void cleanup_module(void) {
+	int i;
+	ssize_t read_ret = 0;
+
 	unregister_chrdev(MAJOR, DEVICE_NAME);
+
+	for (i = 0; i < MINORS; i++) {
+		printk(KERN_INFO "%s: cleaning device #%d\n", MODNAME, i);
+		wake_up_all(&(files[i].wait_queue));
+
+		// calling __read_common with buff == NULL just dumps the message
+		while (__read_common(&files[i], NULL, 1, NULL) > 0);
+	}
 
 	printk(KERN_INFO "%s: unregistered\n", MODNAME);
 	return;
