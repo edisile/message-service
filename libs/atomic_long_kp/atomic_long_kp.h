5235511a268b8c6d3475c0e515d109c40d703ac9
edisile <mantapeppe@gmail.com>
2020-02-16 23:56:08 +0100

    Implemented more sophisticated parameter parsing

--- libs/atomic_long_kp/atomic_long_kp.h
+++ libs/atomic_long_kp/atomic_long_kp.h
@@ -3,21 +3,48 @@
 #include <linux/errno.h>
 #include <linux/moduleparam.h>
 
-static int param_long_atomic_set(const char *val, const struct kernel_param *kp);
+static int param_atomic_long_set(const char *val, const struct kernel_param *kp);
 
 static const struct kernel_param_ops atomic_long_param_ops = {
-	.set = param_long_atomic_set,
+	.set = param_atomic_long_set,
 	.get = param_get_long,
 };
 
-static int param_long_atomic_set(const char *val, const struct kernel_param *kp) {
-	long v;
+static int param_atomic_long_set(const char *val, const struct kernel_param *kp) {
+	long v; // Value for the parameter
+	char c; // Measurement unit, if provided
 	int ret;
 
-	ret = kstrtol(val, 10, &v);
-	if (ret != 0 || v < 0)
+	ret = sscanf(val, "%ld%c", &v, &c);
+
+	if (ret == 0 || v < 0) {
+		printk("Invalid parameter value");
 		return -EINVAL;
-	
+	}
+
+	// A measurement unit might have been passed
+	switch (c) {
+	case 'G':
+	case 'g':
+		v = v << 10;
+		// fall through
+	case 'M':
+	case 'm':
+		v = v << 10;
+		// fall through
+	case 'K':
+	case 'k':
+		v = v << 10;
+		break;
+	case '\n':
+	case '\0':
+		// It was just a number with no unit
+		break;
+	default:
+		printk("Invalid parameter measurement unit; valid ones are kmgKMG");
+		return -EINVAL;
+	}
+
 	printk("Atomically setting parameter to value %ld", v);
 	atomic_long_set((atomic_long_t *) kp->arg, v);
 
